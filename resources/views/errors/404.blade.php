67e9de6836e81dcbafd9e5c8a8ffc8a90276c838
Mate Kovacs <mate.kovacs@ericsson.com>
2016-09-19 17:20:16 +0200

    [UNSTABLE Exceptions] 404 exception handler
    Sync.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app', ['data' => $layout])
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{ $layout->language('error') }}</div>
+                <div class="panel-body">
+                    <p>{{ $layout->language('oops_something_went_wrong') }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
