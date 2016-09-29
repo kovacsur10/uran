22e1d5451ed0eab0908aa9b54fe15a12b62a1648
Máté Kovács <kovacsur10@gmail.com>
2016-09-29 10:04:08 +0200

    [Refactor] Auth, Database and LayoutData error log and comments.

--- app/Classes/Database.php
+++ app/Classes/Database.php
@@ -3,22 +3,62 @@
 namespace App\Classes;
 
 use DB;
+use App\Classes\Logger;
 
+/* Class name: Database
+ *
+ * This class handles the database wrapper
+ * functions.
+ *
+ * Functionality:
+ * 		- transaction handling
+ */
 class Database{
 	
-	public function __construct(){
-	}
-
+// PUBLIC FUNCTIONS
+	
+	/* Function name: beginTransaction
+	 * Input: -
+	 * Output: -
+	 *
+	 * This function starts a new database transaction.
+	 */
 	public static function beginTransaction(){
-		DB::beginTransaction();
+		try{
+			DB::beginTransaction();
+		}catch(Exception $ex){
+			Logger::error_log("Error at line: ".__FILE__.":".__LINE__." (in function ".__FUNCTION__."). Starting a new transaction was not successful! ".$ex->getMessage());
+		}
 	}
 	
+	/* Function name: rollback
+	 * Input: -
+	 * Output: -
+	 *
+	 * This function rollback a database transaction.
+	 */
 	public static function rollback(){
-		DB::rollback();
+		try{
+			DB::rollback();
+		}catch(Exception $ex){
+			Logger::error_log("Error at line: ".__FILE__.":".__LINE__." (in function ".__FUNCTION__."). Rollbacking the changes in a transaction was not successful! ".$ex->getMessage());
+		}
 	}
 	
+	/* Function name: commit
+	 * Input: -
+	 * Output: -
+	 *
+	 * This function commits a database transaction.
+	 */
 	public static function commit(){
-		DB::commit();
+		try{
+			DB::commit();
+		}catch(Exception $ex){
+			Logger::error_log("Error at line: ".__FILE__.":".__LINE__." (in function ".__FUNCTION__."). Committing the changes in a transaction was not successful! ".$ex->getMessage());
+		}
 	}
+	
+// PRIVATE FUNCTIONS
 	
 }
