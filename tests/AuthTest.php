7a6dea86a6c989e50ff6dff1870d9957c8db55ed
Máté Kovács <kovacsur10@gmail.com>
2016-11-14 22:25:15 +0100

    Error fixes.

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -3,12 +3,15 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use App\Http\Controllers\Auth;
+use App\Classes\Auth;
 use Illuminate\Support\Facades\Session;
+use App\Exceptions;
+use App\Exceptions\ValueMismatchException;
+use App\Exceptions\UserNotFoundException;
 
 class AuthTest extends TestCase
 {
-	/** Function name: unitTest_logout
+	/** Function name: test_logout
 	 * 
 	 * This function is testing the logout function of the Auth model.
 	 * 
@@ -16,13 +19,13 @@ class AuthTest extends TestCase
 	 *
 	 * @return void
 	 */
-	public function unitTest_logout(){
+	public function test_logout(){
 		Session::put('user', 'almafa');
 		Auth::logout();
 		$this->assertFalse(Session::has('user'), "Session variable 'user' is set!");
 	}
 	
-	/** Function name: unitTest_login
+	/** Function name: test_login_success
 	 *
 	 * This function is testing the login function of the Auth model.
 	 *
@@ -30,15 +33,61 @@ class AuthTest extends TestCase
 	 *
 	 * @return void
 	 */
-	public function unitTest_login(){
+	public function test_login_success(){
 		$this->assertFalse(Session::has('user'), "Session variable 'user' is set!");
 		try{
-			Auth::login('unittestUser','unittestUser');
+			Auth::login('forUnitTest','forUnittest');
 		}catch(\Exception $ex){
-			$this->fail("Login exception: ".$ex->message);
+			$this->fail("Login exception: ".$ex->getMessage());
 		}
 		$this->assertTrue(Session::has('user'), "Session variable 'user' is not set!");
 		
+		//cleanup
+		Session::forget('user');
+	}
+	
+	/** Function name: test_login_failUsername
+	 *
+	 * This function is testing the login function of the Auth model.
+	 *
+	 * With a successful login, the session has to be set properly.
+	 *
+	 * @return void
+	 */
+	public function test_login_failUsername(){
+		$this->assertFalse(Session::has('user'), "Session variable 'user' is set!");
+		try{
+			Auth::login('invalidUser','invalid_password');
+			$this->fail("Exception was expected!");
+		}catch(UserNotFoundException $ex){
+		}catch(\Exception $ex){
+			$this->fail("Not the expected exception: ".$ex->getMessage());
+		}
+		$this->assertFalse(Session::has('user'), "Session variable 'user' is set!");
+	
+		//cleanup
+		Session::forget('user');
+	}
+	
+	/** Function name: test_login_failPassword
+	 *
+	 * This function is testing the login function of the Auth model.
+	 *
+	 * With a successful login, the session has to be set properly.
+	 *
+	 * @return void
+	 */
+	public function test_login_failPassword(){
+		$this->assertFalse(Session::has('user'), "Session variable 'user' is set!");
+		try{
+			Auth::login('forUnitTest','invalid_password');
+			$this->fail("Exception was expected!");
+		}catch(ValueMismatchException $ex){
+		}catch(\Exception $ex){
+			$this->fail("Not the expected exception: ".$ex->getMessage());
+		}
+		$this->assertFalse(Session::has('user'), "Session variable 'user' is set!");
+	
 		//cleanup
 		Session::forget('user');
 	}
