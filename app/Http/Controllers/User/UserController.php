85031f53b7f1c5c057d9eece70d1a6afe63e3da1
Máté Kovács <kovacsur10@gmail.com>
2017-01-26 07:29:29 +0100

    Sync.

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,23 +5,41 @@ namespace App\Http\Controllers\User;
 use App\Classes\LayoutData;
 use App\Classes\Layout\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Session;
 
-class UserController extends Controller{	
+/** Class name: UserController
+ *
+ * This controller is for handling the user data related things.
+ *
+ * @author Máté Kovács <kovacsur10@gmail.com>
+ */
+class UserController extends Controller{
+	
+	/** Function name: showData
+	 *
+	 * This function shows the user data page.
+	 *
+	 * @author Máté Kovács <kovacsur10@gmail.com>
+	 */
     public function showData(){
 		return view('user.showdata', ["layout" => new LayoutData()]);
 	}
 	
+	/** Function name: showPublicData
+	 *
+	 * This function shows the public user data page.
+	 *
+	 * @author Máté Kovács <kovacsur10@gmail.com>
+	 */
 	public function showPublicData($username){
 		$layout = new LayoutData();
-		$targetId = $layout->user()->getUserDataByUsername($username);
-		if($targetId == null){
+		try{
+			$targetId = $layout->user()->getUserDataByUsername($username);
+			return view('user.showpublicdata', ["layout" => $layout,
+					"target" => new User($targetId->id)]);
+		}catch(\Exception $ex){
 			return view('errors.error', ["layout" => $layout,
 										 "message" => $layout->language('error_at_finding_the_user'),
 										 "url" => '/home']);
-		}else{
-			return view('user.showpublicdata', ["layout" => $layout,
-												"target" => new User($targetId->id)]);
 		}
 	}
 }
--- resources/views/user/showpublicdata.blade.php
+++ resources/views/user/showpublicdata.blade.php
@@ -7,8 +7,8 @@
             <div class="panel panel-default">
                 <div class="panel-heading">{{ $layout->language('my_data') }}</div>
                 <div class="panel-body">
-					<div class="well well-sm">{{ $layout->language('name') }}: {{ $target->user()->name }}</div>
-					<div class="well well-sm">{{ $layout->language('registration_date') }}: {{ $layout->formatDate($target->user()->registration_date) }}</div>
+					<div class="well well-sm">{{ $layout->language('name') }}: {{ $target->user()->name() }}</div>
+					<div class="well well-sm">{{ $layout->language('registration_date') }}: {{ $layout->formatDate($target->user()->registrationDate()) }}</div>
                 </div>
             </div>
         </div>
